afefeff0c119c5b920bc33681a51e8b7fa9d08f1
UuqV <krlovell017@gmail.com>
2022-03-03 23:41:49 -0500

    preparing for packaging

--- src/components/paywall.js
+++ src/components/paywall.js
@@ -1,45 +1,14 @@
-/**
- * Bio component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import colors from '../colors';
 import payWithSol from './send_sol';
 import { Button } from '@mui/material';
 
 const Paywall = (props) => {
-  const data = useStaticQuery(graphql`
-    query Paywall {
-      avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
-        childImageSharp {
-          fluid(quality: 100, maxWidth: 1600) {
-            src
-            srcSet
-            originalImg
-            originalName
-            aspectRatio
-            sizes
-          }
-        }
-      }
-      site {
-        siteMetadata {
-          author
-        }
-      }
-    }
-  `);
-
   return (
     <div
       style={{
         display: `flex`,
         boxShadow: `0px 0px 20px 0px`,
-        background: colors.background,
+        background: `rgb(16, 6, 69)`,
       }}
     >
       <div
